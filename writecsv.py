4538b62b6c57990e621c27a83e942d163331809f
Geng812 <39304725+Geng812@users.noreply.github.com>
2018-10-17 00:07:40 +0800

    Update writecsv.py

--- writecsv.py
+++ writecsv.py
@@ -1,1 +1,12 @@
 # write data to csv
+import csv
+
+with open("stock.csv", "a") as csvfile:
+  writer = csv.writer(csvfile)
+  writer.writerow(smt['title'])
+  writer.writerow(smt['fields'])
+  
+  for data in (s['data']):
+    with open("stock.csv", "a") as csvfile:
+      writer = csv.writer(csvfile)
+      writer.writerow(data)
