4511f4d71db03d5a1e6c69192a2a85b5c46a8ea1
maxwellbkwt <maxwellbkwt@gmail.com>
2018-10-17 22:56:24 +0800

    edit checkfile by try/except

--- checkfile.py
+++ checkfile.py
@@ -2,7 +2,15 @@ import os
 
 path = "/Users/nick/Desktop/2454.csv"
 
-if ( os.path.isfile(path)):
-    print("file exists!")
-else:
-    print("file not found...")
+#if ( os.path.isfile(path)):
+#    print("file exists!")
+#else:
+#    print("file not found...")
+
+def file_exists(path):
+    try:
+        st = os.stat(path)
+    except os.error:
+        return False
+
+    return True
