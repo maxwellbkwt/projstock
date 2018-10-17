ef2790088e6eb52b48b1525ea3187ae06c19415d
maxwellbkwt <maxwellbkwt@gmail.com>
2018-10-17 23:41:31 +0800

    modify dataparse

--- dataparse.py
+++ dataparse.py
@@ -1,13 +1,17 @@
 import requests
-from bs4 import BeautifulSoup as bs
+#from bs4 import BeautifulSoup as bs
 import json
-
+#http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=201801&stockNo=2454
 def get_webmsg (year, month, stock_id):
-    url_twse = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=' + date + '&stockNo=' + sid
     date = str (year) + "{0:0=2d}".format(month) +'01' ## format is yyyymmdd
+    print(date)
     sid = str(stock_id)
+    url_twse = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=' + date + '&stockNo=' + sid
 
     res =requests.post(url_twse,)
-    soup = bs(res.text , 'html.parser')
-    smt = json.loads(soup.text)     #convert data into json
+#    soup = bs(res.text , 'html.parser') #covert to json
+    smt = json.loads(res.text)     #convert data into json
+    fileds = smt['fields']
+    data=smt['data']
+    print(data)
     return smt
